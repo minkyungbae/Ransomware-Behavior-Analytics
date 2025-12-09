8acf4f8d177d2c599c5d44197a19aa9baa2a9a5c
unknown <sea4536@gmail.com>
2025-12-09 09:16:53 +0900

    Update: ransomware/urls.py에 메인 페이지 url 추가

--- backend/ransomware/urls.py
+++ backend/ransomware/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.mainpage, name="mainpage"),
     path("ping/", views.ping),
     path("train/", views.train_models),
     path("classes/", views.get_classes),
